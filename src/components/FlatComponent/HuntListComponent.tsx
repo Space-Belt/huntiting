3cfb4f0cc231a899e6674e6e6b26c5783749ab85
OnlyWooo <dngurwn@gmail.com>
2024-05-11 17:38:40 +0900

    HuntList FlatList에들어갈 리스트 컴포넌트  생성

---
+++ src/components/FlatComponent/HuntListComponent.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const HuntListComponent = (props: Props) => {
+  return (
+    <View>
+      <Text>HuntListComponent</Text>
+    </View>
+  );
+};
+
+export default HuntListComponent;
+
+const styles = StyleSheet.create({});
