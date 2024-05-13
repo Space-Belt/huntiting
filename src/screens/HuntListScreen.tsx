2ee8fc78165e84eb7df93d6902f027ec2c61fe92
OnlyWooo <dngurwn@gmail.com>
2024-05-13 15:46:47 +0900

    헤더 추가

--- src/screens/HuntListScreen.tsx
+++ src/screens/HuntListScreen.tsx
@@ -1,12 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import HuntListComponent from '../components/FlatComponent/HuntListComponent';
+import ReusableHeader from '../components/ReusableHeader';
+import {useNavigation} from '@react-navigation/native';
 
 type Props = {};
 
 const HuntListScreen = (props: Props) => {
+  const navigation = useNavigation();
+
+  const handleBackBtn = () => {
+    navigation.navigate('detail' as never);
+  };
+
   return (
     <View>
-      <Text>HuntListScreen</Text>
+      <ReusableHeader title="HomeScreen" handleBackBtn={handleBackBtn} />
+      <HuntListComponent />
     </View>
   );
 };
