9cffe8ff3186552553d88648891c7d9727403696
OnlyWooo <dngurwn@gmail.com>
2024-05-15 20:31:35 +0900

    페이지 알려주는 헤더추가

--- src/screens/MyPageScreen.tsx
+++ src/screens/MyPageScreen.tsx
@@ -4,13 +4,22 @@ import WholeWrapper from '../components/WholeWrapper';
 import FastImage from 'react-native-fast-image';
 import space from '../assets/images/space.png';
 import MyPageHeader from '../components/MyPage/MyPageHeader';
+import ReusableHeader from '../components/ReusableHeader';
+import {useNavigation} from '@react-navigation/native';
 
 type Props = {};
 
 const MyPageScreen = (props: Props) => {
+  const navigation = useNavigation();
+
+  const handleBackBtn = () => {
+    navigation.goBack();
+  };
   return (
     <WholeWrapper>
       <View style={styles.container}>
+        <ReusableHeader title="프로필" handleBackBtn={handleBackBtn} />
+
         <MyPageHeader />
       </View>
     </WholeWrapper>
