5c8adedcc5cf271e0a67ecab2aca3fefe40a4c79
OnlyWooo <dngurwn@gmail.com>
2024-05-20 17:12:24 +0900

    헤더추가

--- src/screens/HuntDetail.tsx
+++ src/screens/HuntDetail.tsx
@@ -1,17 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import WholeWrapper from '../components/WholeWrapper';
+import ReusableHeader from '../components/ReusableHeader';
 
 type Props = {};
 
 const HuntDetail = (props: Props) => {
   return (
     <WholeWrapper>
-      <Text>HuntDetail</Text>
+      <>
+        <ReusableHeader title={'상세보기'} />
+      </>
     </WholeWrapper>
   );
 };
 
 export default HuntDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageWrapper: {},
+});
