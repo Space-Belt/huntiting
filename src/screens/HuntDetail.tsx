6f710ee065e3acb0cf95d292e5cdfa07632fe75e
OnlyWooo <dngurwn@gmail.com>
2024-05-20 18:22:57 +0900

    캐러셀 적용

--- src/screens/HuntDetail.tsx
+++ src/screens/HuntDetail.tsx
@@ -2,14 +2,39 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import WholeWrapper from '../components/WholeWrapper';
 import ReusableHeader from '../components/ReusableHeader';
+import Carousel from 'react-native-reanimated-carousel';
+import FastImage from 'react-native-fast-image';
+import {getLayout} from '../utils/getLayout';
 
 type Props = {};
 
 const HuntDetail = (props: Props) => {
+  const renderItem = (item: any) => {
+    return (
+      <View>
+        <FastImage source={{uri: item}} style={styles.imageStyle} />
+      </View>
+    );
+  };
+
   return (
     <WholeWrapper>
       <>
         <ReusableHeader title={'상세보기'} />
+        <View>
+          <Carousel
+            loop
+            width={getLayout()}
+            height={getLayout()}
+            autoPlay={true}
+            data={[
+              'https://picsum.photos/400',
+              'https://picsum.photos/400',
+              'https://picsum.photos/400',
+            ]}
+            renderItem={({item}) => renderItem(item)}
+          />
+        </View>
       </>
     </WholeWrapper>
   );
@@ -19,4 +44,9 @@ export default HuntDetail;
 
 const styles = StyleSheet.create({
   imageWrapper: {},
+  imageStyle: {
+    width: getLayout(),
+    height: getLayout(),
+    resizeMode: 'cover',
+  },
 });
--- src/utils/getLayout.ts
+++ src/utils/getLayout.ts
@@ -2,5 +2,5 @@ import {Dimensions, useWindowDimensions} from 'react-native';
 
 export const getLayout = () => {
   const {width, height} = Dimensions.get('window');
-  return {height, width};
+  return width;
 };
