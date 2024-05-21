1d18fc2b850b54dbc3d018020cda7e49524d826b
OnlyWooo <dngurwn@gmail.com>
2024-05-21 17:59:07 +0900

    채팅버튼 추가

--- src/components/Detail/BottomDeal.tsx
+++ src/components/Detail/BottomDeal.tsx
@@ -1,12 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import {COLORS, FONTSIZE} from '../../theme/theme';
 
 type Props = {};
 
 const BottomDeal = (props: Props) => {
   return (
     <View style={styles.container}>
-      <Text>BottomDeal</Text>
+      <Text style={styles.priceText}></Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={() => {}}>
+        <Text style={styles.buttonText}>채팅하기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -15,10 +19,29 @@ export default BottomDeal;
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 15,
-    paddingHorizontal: 10,
+    flexDirection: 'row',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 25,
     backgroundColor: '#e4e4e4ea',
     borderTopWidth: 0.5,
     borderColor: '#eee',
+  },
+  priceText: {
+    color: COLORS.Grey,
+    fontSize: FONTSIZE.size_16,
+    fontWeight: '700',
+  },
+  buttonStyle: {
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    backgroundColor: COLORS.Orange,
+  },
+  buttonText: {
+    color: COLORS.White,
+    fontSize: FONTSIZE.size_14,
+    fontWeight: '800',
   },
 });
