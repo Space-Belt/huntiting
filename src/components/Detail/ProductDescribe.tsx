2baf6d3d8c37c0e0aff0c246b1f5754ba457fecc
OnlyWooo <dngurwn@gmail.com>
2024-05-21 17:18:36 +0900

    shadow 적용

--- src/components/Detail/ProductDescribe.tsx
+++ src/components/Detail/ProductDescribe.tsx
@@ -12,7 +12,13 @@ type Props = {
 const ProductDescribe = ({name, price, description, count}: Props) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.productName}>{name}</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.productName}>{name}</Text>
+        <View style={styles.describeContainer}>
+          <Text style={styles.priceText}>가격: {price}원</Text>
+          <Text style={styles.countText}>수량: {count}개</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -20,13 +26,40 @@ const ProductDescribe = ({name, price, description, count}: Props) => {
 export default ProductDescribe;
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
+  container: {},
+  titleContainer: {
+    backgroundColor: '#eee',
+    paddingVertical: 15,
     paddingHorizontal: 15,
+    borderColor: '#d9d7ba',
+    borderWidth: 1,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 5,
   },
   productName: {
     fontSize: FONTSIZE.size_20,
     fontWeight: '700',
     color: COLORS.Orange2,
+  },
+  describeContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  priceText: {
+    width: '50%',
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.Orange2,
+    fontWeight: '700',
+  },
+  countText: {
+    width: '50%',
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.Orange2,
+    fontWeight: '700',
   },
 });
