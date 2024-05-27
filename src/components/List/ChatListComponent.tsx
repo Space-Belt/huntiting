fdf66da16c8419202074603540f87f35f4386df7
OnlyWooo <dngurwn@gmail.com>
2024-05-27 18:46:32 +0900

    채팅리스트 왼쪽으로 움직이는 로직 추가

--- src/components/List/ChatListComponent.tsx
+++ src/components/List/ChatListComponent.tsx
@@ -5,45 +5,99 @@ import {dateConverter} from '../../utils/dateConverter';
 import {IChatRoom} from '../../screens/ChatList';
 import {FONTSIZE, COLORS} from '../../theme/theme';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {useLayout} from '../../hooks/useLayout';
 
 type Props = {
   handleGotoChatRoom: (id: number) => void;
   item: IChatRoom;
 };
 
+const DEFAULT_WIDTH = 0;
+const DEFAULT_TRANSLATEX = 0;
+
 const ChatListComponent = ({handleGotoChatRoom, item}: Props) => {
+  const translateX = useSharedValue(DEFAULT_TRANSLATEX);
+  const tempTranslateX = useSharedValue(DEFAULT_TRANSLATEX);
+
+  const deleteBtnWidth = useSharedValue(DEFAULT_WIDTH);
+
   const panGestureEvent = Gesture.Pan()
-    .onStart(() => {})
-    .onUpdate(event => {})
+    .onStart(() => {
+      tempTranslateX.value = translateX.value;
+    })
+    .onUpdate(event => {
+      if (tempTranslateX.value === 0 && event.translationX > 0) {
+        return;
+      } else {
+        translateX.value = withTiming(event.translationX, {
+          duration: 0,
+        });
+      }
+    })
     .onEnd(event => {});
 
+  const listAnimatedStyle = useAnimatedStyle(() => {
+    if (translateX.value > 0) {
+      return {
+        transform: [{translateX: 0}],
+      };
+    } else {
+      return {
+        transform: [{translateX: translateX.value}],
+      };
+    }
+  });
+
+  /**
+   * 1. 전체를 제스쳐핸들러로 감싸기
+   * 2. 왼쪽으로 움직이는 위에 올라오는 채팅
+   * 3. 삭제 누를수 있는 버튼 (길어짐)
+   * 4. 삭제버튼은 제스쳐따라 늘어남, 조금만 움직였으면 어느정도 width로 고정,
+   *    많이 움직이면 자동 삭제, 나갈건지 물어보는 모달
+   * 5. 삭제 버튼 누르거나 다른거
+   */
   return (
     <GestureDetector gesture={panGestureEvent}>
-      <TouchableOpacity
-        key={`${item.id}_${item.opponentNickname}`}
-        style={styles.chatListContainer}
-        onPress={() => handleGotoChatRoom(item.id)}>
-        <FastImage
-          source={{uri: item.opponentProfileImg}}
-          style={styles.profileImg}
-        />
-        <View style={styles.messageContainer}>
-          <View style={styles.nickNameWrapper}>
-            <Text style={styles.nickNameText}>{item.opponentNickname}</Text>
-            <Text style={styles.messageCountText}>{item.lastMessageCount}</Text>
-          </View>
-          <View style={styles.messageWrapper}>
-            <Text style={styles.messageText}>
-              {item.lastMessage.length > 18
-                ? item.lastMessage.slice(0, 18) + '...'
-                : item.lastMessage}
-            </Text>
-            <Text style={styles.timeWrapper}>
-              {dateConverter(item.lastMessageTime)}
-            </Text>
-          </View>
-        </View>
-      </TouchableOpacity>
+      <View>
+        <TouchableOpacity
+          key={`${item.id}_${item.opponentNickname}`}
+          onPress={() => handleGotoChatRoom(item.id)}>
+          <Animated.View style={[styles.chatListContainer, listAnimatedStyle]}>
+            <FastImage
+              source={{uri: item.opponentProfileImg}}
+              style={styles.profileImg}
+            />
+            <View style={styles.messageContainer}>
+              <View style={styles.nickNameWrapper}>
+                <Text style={styles.nickNameText}>{item.opponentNickname}</Text>
+                <Text style={styles.messageCountText}>
+                  {item.lastMessageCount}
+                </Text>
+              </View>
+              <View style={styles.messageWrapper}>
+                <Text style={styles.messageText}>
+                  {item.lastMessage.length > 18
+                    ? item.lastMessage.slice(0, 18) + '...'
+                    : item.lastMessage}
+                </Text>
+                <Text style={styles.timeWrapper}>
+                  {dateConverter(item.lastMessageTime)}
+                </Text>
+              </View>
+            </View>
+          </Animated.View>
+        </TouchableOpacity>
+        <Animated.View>
+          <TouchableOpacity onPress={() => {}}>
+            <Text>삭제버튼</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
     </GestureDetector>
   );
 };
