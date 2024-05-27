2f41f75249faf8e1f04d1a5c7d8555530b981e08
OnlyWooo <dngurwn@gmail.com>
2024-05-27 17:11:36 +0900

    모달 애니메이션 타입 props 적용

--- src/components/ReusableModal.tsx
+++ src/components/ReusableModal.tsx
@@ -1,18 +1,19 @@
-import {Modal, StyleSheet} from 'react-native';
+import {Modal, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import React from 'react';
 
 interface IProps {
   visible: boolean;
   onClose: () => void;
   children: React.ReactElement;
+  animationType: 'fade' | 'none' | 'slide';
 }
 
-const ReusableModal = ({visible, onClose, children}: IProps) => {
+const ReusableModal = ({visible, onClose, children, animationType}: IProps) => {
   return (
     <Modal
       visible={visible}
       transparent={true}
-      animationType="fade"
+      animationType={animationType}
       onRequestClose={onClose}>
       {children}
     </Modal>
