8ced9c1beb3b9540ea2751867c1e3d07e5e07372
OnlyWooo <dngurwn@gmail.com>
2024-05-29 16:47:22 +0900

    모달 스타일 변경 및 수정

--- src/components/Modal/ChatRoomOutModal.tsx
+++ src/components/Modal/ChatRoomOutModal.tsx
@@ -7,32 +7,37 @@ import {
 } from 'react-native';
 import React from 'react';
 import {FONTSIZE} from '../../theme/theme';
+import {getPlatform} from '../../utils/getPlatform';
 
 interface IProps {
   onClose: (args?: string) => void;
+  type: string;
 }
 
-const ChatRoomOutModal = ({onClose}: IProps) => {
+const ChatRoomOutModal = ({onClose, type}: IProps) => {
   return (
-    <TouchableWithoutFeedback onPress={onClose}>
+    <TouchableWithoutFeedback onPress={() => onClose()}>
       <View style={styles.background}>
         <View style={styles.whiteBox}>
-          <Pressable
-            style={styles.actionButton}
-            android_ripple={{color: '#eee'}}
-            onPress={() => {
-              onClose('exit');
-            }}>
-            <Text style={styles.actionText}>채팅방 나가기</Text>
-          </Pressable>
-          <Pressable
-            style={styles.actionButton}
-            android_ripple={{color: '#eee'}}
-            onPress={() => {
-              onClose();
-            }}>
-            <Text style={styles.actionText}>취소</Text>
-          </Pressable>
+          <Text style={styles.noticeWord}>이 채팅방을 나가시겠습니까?</Text>
+          <View style={styles.btnContainer}>
+            <Pressable
+              style={styles.actionButton}
+              android_ripple={{color: '#eee'}}
+              onPress={() => {
+                onClose(type);
+              }}>
+              <Text style={styles.actionText}>채팅방 나가기</Text>
+            </Pressable>
+            <Pressable
+              style={styles.secondActionButton}
+              android_ripple={{color: '#eee'}}
+              onPress={() => {
+                onClose();
+              }}>
+              <Text style={styles.actionText}>취소</Text>
+            </Pressable>
+          </View>
         </View>
       </View>
     </TouchableWithoutFeedback>
@@ -44,7 +49,6 @@ export default ChatRoomOutModal;
 const styles = StyleSheet.create({
   background: {
     backgroundColor: '#00000092',
-
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -55,10 +59,30 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 2,
   },
+  btnContainer: {
+    flexDirection: 'row',
+  },
+  noticeWord: {
+    textAlign: 'center',
+    fontSize: getPlatform() === 'ios' ? FONTSIZE.size_16 : 12,
+    paddingVertical: 20,
+  },
   actionButton: {
     padding: 16,
     flexDirection: 'row',
     alignItems: 'center',
+    flex: 1,
+    borderTopWidth: 0.5,
+    borderBottomLeftRadius: 15,
+  },
+  secondActionButton: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    borderLeftWidth: 0.5,
+    borderTopWidth: 0.5,
+    borderBottomRightRadius: 15,
   },
   iconStyle: {
     fontSize: FONTSIZE.size_18,
@@ -67,8 +91,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 26,
     color: 'white',
+    textAlign: 'center',
   },
   actionText: {
     color: 'black',
+    textAlign: 'center',
+    flex: 1,
+    fontSize: getPlatform() === 'ios' ? FONTSIZE.size_14 : FONTSIZE.size_10,
   },
 });
